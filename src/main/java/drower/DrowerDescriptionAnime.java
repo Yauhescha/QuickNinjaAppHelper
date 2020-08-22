67eccfecdf15c6b641092c5f6a2417794a9c696e
Dell <Dell@DESKTOP-RF39OKA>
2020-08-22 21:24:29 +0300

    немного рефактора кода

--- src/main/java/drower/DrowerDescriptionAnime.java
+++ src/main/java/drower/DrowerDescriptionAnime.java
@@ -1,4 +1,5 @@
 package main.java.drower;
+
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -6,18 +7,24 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DrowerDescriptionAnime {
-	public static final String FOLDER_WITH_FOLREDS = "D:\\anime-description";
-	public static String fileDescription = "description.txt";
+	public String fileDescription;
+
 	private ArrayList<File> folderList = new ArrayList<File>();
 	private String separator = "\\";
-	private String FolderToSave = "description";
+	private String FolderToSave;
+
 	private DrowerImage drowerImage = new DrowerImage();
 
 	public static void main(String[] args) {
 		DrowerDescriptionAnime drower = new DrowerDescriptionAnime();
-		
-//		drower.getFilesInFolder(FOLDER_WITH_FOLREDS);
-		drower.getFilesInFolder(new Scanner(System.in).nextLine());
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Please, write filename.txt file with text to image");
+		drower.setFileDescription(scanner.nextLine());
+		System.out.println("Please, write path to save file");
+		drower.setFolderToSave(scanner.nextLine());
+		System.out.println("Please, write path to directory with folders with text to image");
+		drower.getFilesInFolder(scanner.nextLine());
 		drower.goAllFolder();
 	}
 
@@ -37,7 +44,7 @@ public class DrowerDescriptionAnime {
 			String readText = readText(folder);
 			BufferedImage image = drowerImage.draw(readText);
 			DrowerImage.saveImage(image, FolderToSave + separator + folder.getName(), "png");
-			
+
 			System.out.println("Step: " + i);
 		}
 	}
@@ -53,6 +60,14 @@ public class DrowerDescriptionAnime {
 			e.printStackTrace();
 		}
 		return text;
+	}
+
+	public void setFileDescription(String fileDescription) {
+		this.fileDescription = fileDescription;
+	}
+
+	public void setFolderToSave(String folderToSave) {
+		FolderToSave = folderToSave;
 	}
 
 }
